e5fa57d5f95a6f52316c51c010547a59ee049952
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2025-04-18 23:51:07 +0530

    Time: 44 ms (36.84%), Space: 44.8 MB (78.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {  // start j from i+1 to avoid using the same element
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };  // Return the indices of the two numbers
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] {i,j};
                 }
             }
         }
-        throw new IllegalArgumentException("No two sum solution found");
+        return new int[0];
     }
 }
