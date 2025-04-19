20392f1479c208df294e821305d3a77ef71c9c19
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2025-04-19 15:21:40 +0530

    Time: 0 ms (100%), Space: 40.5 MB (92.93%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int num) {
+        int slow = num;
+        int fast = getSumOfSquares(num);
+
+        while (fast != 1 && slow != fast) {
+            slow = getSumOfSquares(slow);
+            fast = getSumOfSquares(getSumOfSquares(fast));
+        }
+
+        return fast == 1;
+    }
+
+    private int getSumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
