930286b1bb5b71a38e7713ffae039124b07281a8
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2025-05-19 13:10:00 +0530

    Time: 5 ms (70.7%), Space: 44 MB (75.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,13 @@
 class Solution{
     public boolean isPalindrome(int num){
-        int rev=0;
         int temp=num;
-        while(num!=0 && num>0){
+        int rev=0;
+        while(num>0){
             int digit=num%10;
             rev=rev*10+digit;
-            num=num/10;
+            num/=10;
         }
-        if(rev==temp){
+        if(temp==rev){
             return true;
         }
         else{
