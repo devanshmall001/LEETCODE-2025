55915a2ac515cc930b5a41a1bdb5e304cf8ded40
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2025-05-20 21:06:31 +0530

    Time: 0 ms (100%), Space: 40.5 MB (39.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,18 @@
 class Solution{
-    public int fib(int n){
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        if(n>=1){
-            dp[1]=1;
+    public int fib(int num){
+        if(num==0){
+            return 0;
         }
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        else if(num==1){
+            return 1;
         }
-        return dp[n];
+        int first_term=0;
+        int second_term=1;
+        for(int i=1;i<=num;i++){
+            int third_term=first_term+second_term;
+            first_term=second_term;
+            second_term=third_term;
+        }
+        return first_term;
     }
 }
