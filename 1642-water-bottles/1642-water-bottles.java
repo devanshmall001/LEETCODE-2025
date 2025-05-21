c8972e762d84deb154f2faca962e9205a1b8f89c
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2025-05-21 19:25:12 +0530

    Time: 0 ms (100%), Space: 40.2 MB (84.04%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,15 +1,12 @@
-class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int sum = 0;
-        int emptyBottles = 0;
-        
-        while (numBottles > 0) {
-            sum += numBottles; // Drink all current bottles
-            emptyBottles += numBottles; // Add to empty bottles
-            numBottles = emptyBottles / numExchange; // Get new full bottles
-            emptyBottles = emptyBottles % numExchange; // Remaining empty bottles
+class Solution{
+    public int numWaterBottles(int numBottles,int numExchange){
+        int ans=numBottles;
+        while(numBottles>=numExchange){
+            int newBottles=numBottles/numExchange;
+            ans=ans+newBottles;
+            int remainBottles=numBottles%numExchange;
+            numBottles=newBottles+remainBottles;
         }
-        
-        return sum;
+        return ans;
     }
 }
