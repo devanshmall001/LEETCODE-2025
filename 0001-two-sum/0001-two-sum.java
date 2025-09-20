5c5f3e3aef5173f508b13afeec71c714d7e6d7e9
DEVANSH MALL <118126285+devanshmall001@users.noreply.github.com>
2025-09-20 23:06:24 +0530

    Time: 35 ms (45.35%), Space: 45 MB (65.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,15 @@
 class Solution{
-    public int [] twoSum(int arr[], int target){
-        int ar[]=new int[2];
+    public static int[] twoSum(int arr[],int target){
+        int ans[]=new int[2];
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i]+arr[j]==target){
-                    ar[0]=i;
-                    ar[1]=j;
+                    ans[0]=i;
+                    ans[1]=j;
                     break;
                 }
             }
         }
-        return ar;
+        return ans;
     }
 }
